c71f554adb64522b76c2543089722d31baa48137
Helios <3023016345@qq.com>
2025-06-08 11:42:42 +0800

    -修改txt的读取方式（允许输入中文名称文件） -增加敏感词库

--- sensitive_word_replace_c.c
+++ sensitive_word_replace_c.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <wchar.h>
 
 long get_file_size(FILE* file_ptr) {
     // 获取文件大小
@@ -14,7 +14,7 @@ long get_file_size(FILE* file_ptr) {
 
 int main() {
     // 打开文件
-    FILE* file_ptr = fopen("D:\\my_python_projects\\sensitive_word_replace_c\\test_novel.txt", "rb");
+    FILE* file_ptr = _wfopen(L"重生80.txt", L"rb");
     if (file_ptr == NULL) {
         printf("File open failed.\n");
         return 1;
@@ -23,29 +23,23 @@ int main() {
     // 读取文件内容
     long file_size = get_file_size(file_ptr);
 
-    // 为文件内容分配内存
-    char* novel = (char*)malloc(file_size+1);
-    if (novel == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
+    // 定义字符串
+    char novel_content[file_size+10];
 
     // 读取文件内容（字符串）到内存中
     int i = 0;
     char get_char;
     while((get_char = fgetc(file_ptr)) != EOF && i < file_size) {
-        novel[i++] = (char)get_char;
+        novel_content[i++] = (char)get_char;
     }
-    novel[i] = '\0';
+    novel_content[i] = '\0';
 
     // 关闭文件
     fclose(file_ptr);
 
     // 输出文件内容
-    printf("%s\n", novel);
+    printf("%s\n", novel_content);
 
-    // 释放内存
-    free(novel);
 
     return 0;
 }
